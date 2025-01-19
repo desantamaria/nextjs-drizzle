2a915b9cd2dccdadc101ab3a237277c754e29b5b
Daniel Santamaria <santamaria.daniel316@gmail.com>
2025-01-18 17:45:17 -0800

    Remove Boilerplate

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  //   webpack: config => {
+  //     config.experiments = {
+  //       ...config.experiments,
+  //       asyncWebAssembly: true,
+  //       layers: true,
+  //     };
+  //     return config;
+  //   },
+};
 
 export default nextConfig;
